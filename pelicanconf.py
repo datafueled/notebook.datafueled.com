1eb32ba7234241321335ae0ab5dd3b58588cb9b2
Bruce Mitchener <bruce.mitchener@gmail.com>
2014-09-03 16:17:16 +0700

    Fix up some basic configuration.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,9 +8,15 @@ SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'UTC'
 
 DEFAULT_LANG = u'en'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
