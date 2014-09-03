652bdd7c99cfbdaaee017d6989cfaf3380bc9767
Bruce Mitchener <bruce.mitchener@gmail.com>
2014-09-03 17:34:31 +0700

    Update to new elegant theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,12 +30,40 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/ArmyOfBruce'),
+          ('Github', 'https://github.com/waywardmonkeys'),
+          ('GitTip', 'https://gratipay.com/waywardmonkeys/'),
+          ('Email', 'mailto:bruce.mitchener@gmail.com'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'pelican-elegant-1.3'
+PLUGIN_PATH = '../pelican-plugins/'
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search',
+           'neighbors', 'related_posts', 'share_post',
+           'multi_part']
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+
+THEME = 'themes/elegant'
+
+# Elegant theme
+STATIC_PATHS = ['theme/images', 'images']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search', '404'))
+TAG_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
